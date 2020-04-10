c5578a0b2aa02c594d5f9bdfbd34ecf200ea6c76
aashijitM@gmail.com <aashijitM@gmail.com>
2020-04-11 00:59:35 +0530

    Added the first dao method

---
+++ src/DAO/CardBookInfoDao.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DAO;
+
+use App\DataObject\CardBookInfo;
+use Cake\Datasource\ConnectionManager;
+use Cake\Log\Log;   
+
+class CardBookInfoDao {
+
+    public function CardBookInfoDao(){
+
+    }
+
+    public function getCardBookInfosByCardBookId($cardBookId){
+
+        $conn = ConnectionManager::get('default');
+
+        $sql="SELECT `CARD_ID`, `TAG_ID`, `OCCASSION_ID`, `CATEGORY_ID`, `IS_PRESENT`, `LAST_MAINT_ID`, `LAST_MAINT_TS`, `OPTCOUNTER` FROM `card_book_info` WHERE 1";
+        $stmt = $conn->execute($sql);
+        $results = $stmt->fetchAll();
+
+        
+        $cardBookInfos = [];
+
+        foreach($results as $result){
+            $cardBookInfo = new CardBookInfo;    
+            $cardBookInfo->cardId = $result[0];
+            $cardBookInfo->tagId = $result[1];
+            $cardBookInfo->occassionId = $result[2];
+            $cardBookInfo->categoryId = $result[3];
+            $cardBookInfo->isPresent = $result[4];
+            $cardBookInfo->lastMaintTs = $result[5];
+            $cardBookInfo->lastMaintId = $result[6];
+            $cardBookInfo->optCounter = $result[7];
+            
+            array_push($cardBookInfos,$cardBookInfo);
+        }
+
+        return $cardBookInfos;
+    }
+
+
+}
---
+++ src/DataObject/CardBookInfo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\DataObject;
+
+class CardBookInfo {
+
+    public $cardId;
+    public $tagId;
+    public $occassionId;
+    public $categoryId;
+    public $isPresent;
+    public $lastMaintTs;
+    public $lastMaintId;
+    public $optCounter;
+    
+}
